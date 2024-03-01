ea7e0127eda5039fe4c6fbee6c15a424587da0cd
Varun Nair <vnairr314@gmail.com>
2024-03-01 15:37:46 +0530

    Initial Commit

---
+++ HiveV2/HiveV2App.swift
@@ -0,0 +1,17 @@
+//
+//  HiveV2App.swift
+//  HiveV2
+//
+//  Created by Varun Nair on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HiveV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
